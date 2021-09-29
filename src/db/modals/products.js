83bb5dfed8fc4d1869dae196cdf227b5b388ccb2
Bimal27 <bimalsah1996@gmail.com>
2021-09-29 20:07:44 +0545

    CRUD and join

--- src/db/modals/products.js
+++ src/db/modals/products.js
@@ -32,4 +32,4 @@ const Product = sequelize.define(
   },
 );
 
-export default Author
+export default Product
--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@
 import express from "express";
 import cors from "cors";
 import { connectDB } from "./db/index.js";
+import productsRouter from "./services/products/index.js";
+import reviewsRouter from "./services/reviews/index.js";
 const server = express();
 
 const { PORT = 5000 } = process.env;
@@ -9,6 +11,8 @@ const { PORT = 5000 } = process.env;
 server.use(cors());
 
 server.use(express.json());
+server.use("/products", productsRouter)
+server.use("/reviews", reviewsRouter)
 
 server.listen(PORT, async () => {
   await connectDB();
